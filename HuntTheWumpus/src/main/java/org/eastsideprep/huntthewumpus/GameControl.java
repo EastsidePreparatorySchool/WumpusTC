e66fc41032eaecf147efaf6212b2626543d20000
gunnarmein <gmein@eastsideprep.org>
2020-03-12 11:31:49 -0700

    Example of communicating through interfaces

---
+++ HuntTheWumpus/src/main/java/org/eastsideprep/huntthewumpus/GameControl.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.eastsideprep.huntthewumpus;
+
+import org.eastsideprep.htw_interfaces.Sound;
+
+/**
+ *
+ * @author gmein
+ */
+public class GameControl {
+
+    void test() {
+        Sound s = new SoundClass();
+
+        s.playAlarm();
+        
+    }
+}
---
+++ HuntTheWumpus/src/main/java/org/eastsideprep/huntthewumpus/SoundClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.eastsideprep.huntthewumpus;
+
+import org.eastsideprep.htw_interfaces.Sound;
+
+
+/**
+ *
+ * @author gmein
+ */
+public class SoundClass implements Sound{
+
+    @Override
+    public void playAlarm() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
